c7da2e43a0f996e148af3154fd338d7b684a6a4f
JEANN DA ROCHA SILVA <B43257@fgv.edu.br>
2024-03-25 08:11:12 -0300

    Item 2: fatorial iterativo

--- atividade.cpp
+++ atividade.cpp
@@ -6,10 +6,11 @@ using std::endl;
 using std::string;
 
 int pow(int, int);
+int fatorial(int);
 
 int main()
 {
-    int iBase, iExpoente;
+    int iBase, iExpoente, iFatorial;
     
     cout << "Insira a base: ";
     cin >> iBase;
@@ -17,10 +18,21 @@ int main()
     cout << "Insira o expoente: ";
     cin >> iExpoente;
     
-    int iResultado = pow(iBase, iExpoente);
+    int iResultado1 = pow(iBase, iExpoente);
     
-    cout << "Resultado da exponenciação: " << iResultado << endl;
+    cout << "Resultado da exponenciação: " << iResultado1 << endl;
     
+    cout << "===============================================" << endl;
+
+    cout << "Insira o número (para o fatorial): ";
+    cin >> iFatorial;
+
+    int iResultado2 = fatorial(iFatorial);
+
+    cout << "Resultado do fatorial: " << iResultado2 << endl;
+
+    cout << "===============================================" << endl;
+
     return 0;
 }
 
@@ -34,4 +46,21 @@ int pow(int iBase, int iExpoente)
     }
     
     return iResultado;
+}
+
+
+int fatorial(int iNum)
+{
+
+    if (iNum == 0) return 1;
+
+    int iFatorial = 1;
+
+    for (int i = 1; i <= iNum; i++)
+    {
+        iFatorial *= i;
+    }
+
+    return iFatorial;
+
 }
